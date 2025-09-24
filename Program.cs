e7648885b5c48595c27a2521c33f1d1730866fe8
ekrajamil27 <alzyfatima@gmail.com>
2025-09-24 09:02:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+      
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+
+            
+            if (i == 5)
+            {
+                Thread.Sleep(500); 
+            }
+            else
+            {
+                
+                Thread.Sleep(1000); 
+            }
+        }
+
+     
+        Console.WriteLine($"Welcome {userName}!");
+
+       
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
+
+
